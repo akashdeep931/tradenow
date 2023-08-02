d36314ac049bec0c467368fb97dd77e20a0ce782
akashdeep931 <akashdeepsingh931@gmail.com>
2023-08-03 00:36:05 +0100

    implement home first view

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,36 @@
-
+import { useContext, useEffect, useState } from "react";
+import { fetchAuth } from "../api";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import { GridLoader } from "react-spinners";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchAuth()
+      .then((res) => {
+        setUser(res.data.user);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        navigate("/login");
+      });
+  }, []);
+
+  return (
+    <main className="bg-[#03001C] w-full h-screen">
+      <section className="w-full h-full flex justify-center items-center">
+        {isLoading ? (
+          <GridLoader color="rgba(187, 191, 200, 1)" />
+        ) : (
+          <h1 className="text-[32px] text-[#bbbfc8]">Coming Soon...</h1>
+        )}
+      </section>
+    </main>
+  );
 };
 
 export default Home;
